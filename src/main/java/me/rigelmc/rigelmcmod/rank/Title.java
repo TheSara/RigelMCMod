13fe0f28e5e34570c300ea2aa5af0c0a1291c73f
LightWarp <godofinsanity2000@gmail.com>
2017-05-13 12:48:02 +0530

    Ranks still broken, fuck me

--- src/main/java/me/rigelmc/rigelmcmod/rank/Title.java
+++ src/main/java/me/rigelmc/rigelmcmod/rank/Title.java
@@ -5,10 +5,13 @@ import org.bukkit.ChatColor;
 
 public enum Title implements Displayable
 {
+    SUPER_ADMIN("a", "Super Admin", ChatColor.AQUA, "SA"),
+    TELNET_ADMIN("a", "Telnet Admin", ChatColor.DARK_GREEN, "STA"),
+    SENIOR_ADMIN("a", "Senior Admin", ChatColor.GOLD, "SRA"),
     MASTER_BUILDER("a", "Master Builder", ChatColor.DARK_AQUA, "Master-Builder"),
     DONOR("a", "Donator", ChatColor.YELLOW, "Donator"),
     DEVELOPER("a", "TotalFreedom Developer", ChatColor.DARK_PURPLE, "TF-Dev"),
-    UMCDEV("an", "UnraveledMC Developer", ChatColor.DARK_PURPLE, "UMC-Dev"),
+    UMCDEV("an", "RigelMC Developer", ChatColor.DARK_PURPLE, "RMC-Dev"),
     EXEC("an", "Executive Admin", ChatColor.DARK_BLUE, "Executive"),
     RETIRED("", "Retired", ChatColor.GRAY, "Retired"),
     COOWNER("a", "Co-Owner", ChatColor.DARK_RED, "Co-Owner"),
