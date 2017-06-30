986f43ce9371a8347bdcf1250e950a214e208270
Madhav Kothandarman <godofinsanity2000@gmail.com>
2017-06-30 06:54:56 -0700

    made all donate message all aqua

--- src/main/java/me/rigelmc/rigelmcmod/command/Command_donate.java
+++ src/main/java/me/rigelmc/rigelmcmod/command/Command_donate.java
@@ -16,9 +16,9 @@ public class Command_donate extends FreedomCommand
     {
         msg("If you wanna donate read below:", ChatColor.YELLOW);
         msg("Hey you, you want to donate?", ChatColor.AQUA);
-        msg("LightWarp has to keep the server up, so your donation would be helpful.", ChatColor.DARK_AQUA);
-        msg(" - Go to http://rigelmc.ga/donate/ to Donate. You can donate any amount!", ChatColor.DARK_AQUA);
-        msg(" - Be sure to leave your Minecraft name in the Payment notes text field, so we know that you donated!", ChatColor.DARK_AQUA);
+        msg("LightWarp has to keep the server up, so your donation would be helpful.", ChatColor.AQUA);
+        msg(" - Go to http://rigelmc.ga/donate/ to Donate. You can donate any amount!", ChatColor.AQUA);
+        msg(" - Be sure to leave your Minecraft name in the Payment notes text field, so we know that you donated!", ChatColor.AQUA);
         msg(" - After sending the payment, DM LightWarp#5690 on Discord or wait for him to to look at his Paypal account.", ChatColor.AQUA);
         msg(" - He will give you a special donator rank.", ChatColor.AQUA);
         return true;
